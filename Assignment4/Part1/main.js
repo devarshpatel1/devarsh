c8b516285f071b95db1c57ba7d6d0a3104f4b972
Devarsh Patel <devarsh.patel1@dcmail.ca>
2024-07-30 13:01:21 -0400

    main.js file added and Java script file is linked with Html file
    - main.js file added and link with html file

---
+++ Assignment4/Part1/main.js
@@ -0,0 +1,4 @@
+// Name : Devarsh Patel
+// File : main.js
+// Date : 2024-07-20
+// Description: Silly story generator exercise For MDN Docs : https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator
