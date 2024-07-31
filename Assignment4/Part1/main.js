64aa1ec775de91e59b460eb472d9bfa856452bf5
Devarsh Patel <devarsh.patel1@dcmail.ca>
2024-07-31 12:15:37 -0400

    Add an event listener to the randomize button
    - Add an event listener to the randomize button

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -23,6 +23,8 @@ const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
+// Add an event listener to the randomize button
+randomize.addEventListener('click', result);
 
 // Function to generate a new story.
 function result() {
