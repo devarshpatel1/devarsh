9fce8a222fd37f6f0b9e5877546ed4ce0db39225
Devarsh Patel <devarsh.patel1@dcmail.ca>
2024-07-31 12:14:46 -0400

    Add Result function to generate new story
    - Add result function to generate new story

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -22,3 +22,37 @@ const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. W
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+
+// Function to generate a new story.
+function result() {
+    let newStory = storyText;
+
+    // Get random values to replace placeholders
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    // Replace placeholders in the story text with random values
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
+  
+    // If a custom name is entered, replace 'Bob' with the custom name
+    if (customName.value !== '') {
+      const name = customName.value;
+      newStory = newStory.replaceAll('Bob', name);
+    }
+  
+    // If the UK radio button is checked, convert weights and temperatures to UK units
+    if (document.getElementById("uk").checked) {
+      const weight = `${Math.round(300*0.0714286)} stone`;
+      const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+      newStory = newStory.replaceAll('94 fahrenheit', temperature);
+      newStory = newStory.replaceAll('300 pounds', weight);
+    }
+  
+    // Display the new story
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+  }
