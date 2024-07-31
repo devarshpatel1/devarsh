c2b6865e8dea6f0bd87b01c28b2832e80ccce219
Devarsh Patel <devarsh.patel1@dcmail.ca>
2024-07-31 12:10:09 -0400

    Create const to get reference from html document and create function to get random value from arrays
    - Create const to Get references to elements in the HTML document
    -  Add Function to get a random value from an array
    -  Create Original story text with placeholders
    - Create Arrays of possible values to replace placeholders in the story

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -1,4 +1,24 @@
 // Name : Devarsh Patel
 // File : main.js
 // Date : 2024-07-20
-// Description: Silly story generator exercise For MDN Docs : https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator
+// Description: Silly story generator exercise For MDN Docs : https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator 
+
+
+// Create const to Get references to elements in the HTML document
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+// Function to get a random value from an array
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// Original story text with placeholders
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+// Arrays of possible values to replace placeholders in the story
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
