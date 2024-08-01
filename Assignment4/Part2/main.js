fc7d1c968590a8807b9b31e65d163f1a9edab985
Devarsh Patel <devarsh.patel1@dcmail.ca>
2024-07-31 23:13:30 -0400

    Java file added and linked with html file
    - Create main.js file and linked with html documents
    - Comments added in javascript file

---
+++ Assignment4/Part2/main.js
@@ -0,0 +1,5 @@
+// Name : Devarsh Patel
+// File : index.html
+// Date : 2024-07-21
+// Description: Image gallery exercise For MDN Docs :https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery
+
